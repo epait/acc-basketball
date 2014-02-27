2c2fe059b8ee1a1ff66c1708ef1ee38e7dc35f1d
epait <ericpait@gmail.com>
2014-02-26 21:36:12 -0500

    add players to scraper

--- roster/management/commands/scrape.py
+++ roster/management/commands/scrape.py
@@ -34,33 +34,96 @@ class Command(BaseCommand):
 			tabledata = soup.find('table') #find the proper table
 			team_names = [] #list to store every player in the table
 			team_links = []
-			stat_links = []
 			team_count = 0
 			team_conference_records = []
 			team_overall_records = []
 			team_conference = 'ACC'
+			team_stats_links = []
 
-			for link in tabledata.select('a'):
+			for link in tabledata.find_all('a'):
 				team_links.append(link.get('href'))
 				team_names.append(link.get_text())
+
 				name = link.get_text()
 				team_conference_records.append(soup.find(text=name).next.text)
 				team_overall_records.append(soup.find(text=name).next.next.next.next.next.next.next.text)
 
-
-
-			for team_link, val in enumerate(team_links):
-				# print team_link, val, team_count
-				response = urllib2.urlopen('http://espn.go.com', val)
+				team_url = link.get('href')
+				response = urllib2.urlopen(team_url)
 				html = response.read()
-				soup = BeautifulSoup
-				print team_names[team_count]
-				team_data = Team.objects.create(name= team_names[team_count], conference= team_conference, conference_record= team_conference_records[team_count], overall_record= team_overall_records[team_count])
-				team_data.save()
+				soup = BeautifulSoup(html)
+
+				stats = soup.find('a', text='Statistics')
+				team_stats_links.append(stats.get('href'))
+				# response = urllib2.urlopen('http://espn.go.com', stats_url)
+				# html = response.read()
+				# soup = BeautifulSoup(html)
+				# player_data = soup.find('table')
+				# player_names = []
+
+				# for player in player_data.find_all('a'):
+				# 	player_names.append(player.get_text())
+
+
+
+
+
+			for team_stats_link, val in enumerate(team_stats_links):
+				# print team_link, val, team_count
+				response = urllib2.urlopen('http://espn.go.com%s' % (val), val)
+				html = response.read()
+				soup = BeautifulSoup(html, 'html.parser')
+
+				playerdata = soup.find('table')
+				player_names = []
+				player_count = 0
+				player_ppg = []
+				player_rpg = []
+				player_apg = []
+				player_spg = []
+				player_bpg = []
+				player_tpg = []
+				player_fgp = []
+				player_ftp = []
+				player_tpp = []
+
+				for player in playerdata.select('.evenrow a'):
+					player_names.append(player.get_text())
+					name = player.get_text()
+					player_ppg.append(soup.find(text=name).next.next.next.next.next.text)
+					player_rpg.append(soup.find(text=name).next.next.next.next.next.next.next.text)
+					player_apg.append(soup.find(text=name).next.next.next.next.next.next.next.next.next.text)
+					player_spg.append(soup.find(text=name).next.next.next.next.next.next.next.next.next.next.next.text)
+					player_bpg.append(soup.find(text=name).next.next.next.next.next.next.next.next.next.next.next.next.next.text)
+					player_tpg.append(soup.find(text=name).next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.text)
+					player_fgp.append(soup.find(text=name).next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.text)
+					player_ftp.append(soup.find(text=name).next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.text)
+					player_tpp.append(soup.find(text=name).next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.text)
+					player_count += 1
+
+				for player in playerdata.select('.oddrow a'):
+					player_names.append(player.get_text())
+					name = player.get_text()
+					player_ppg.append(soup.find(text=name).next.next.next.next.next.text)
+					player_rpg.append(soup.find(text=name).next.next.next.next.next.next.next.text)
+					player_apg.append(soup.find(text=name).next.next.next.next.next.next.next.next.next.text)
+					player_spg.append(soup.find(text=name).next.next.next.next.next.next.next.next.next.next.next.text)
+					player_bpg.append(soup.find(text=name).next.next.next.next.next.next.next.next.next.next.next.next.next.text)
+					player_tpg.append(soup.find(text=name).next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.text)
+					player_fgp.append(soup.find(text=name).next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.text)
+					player_ftp.append(soup.find(text=name).next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.text)
+					player_tpp.append(soup.find(text=name).next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.text)
+					player_count += 1
+
+				print team_names[team_count], player_names[0], player_ppg[0], player_rpg[0], player_apg[0], player_spg[0], player_bpg[0], player_tpg[0], player_fgp[0], player_ftp[0], player_tpp[0]
+
+			# 	team_data = Team.objects.create(name= team_names[team_count], conference= team_conference, conference_record= team_conference_records[team_count], overall_record= team_overall_records[team_count])
+			# 	team_data.save()
 				team_count += 1
 
 
 			# print team_names
+			# print player_names
 			# print team_conference_records
 			# print team_overall_records
 
